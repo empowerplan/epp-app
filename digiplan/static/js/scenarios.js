a4083da74d0af98255d655bb3ca6c8206ddfd538
nesnoj <jonathan.amme@rl-institut.de>
2024-03-01 00:59:18 +0100

    Adjust scenarios and add some content for brb2030

--- digiplan/static/js/scenarios.js
+++ digiplan/static/js/scenarios.js
@@ -1,7 +1,7 @@
 import {getCurrentMenuTab} from "./menu.js";
 
 let currentScenario = null;
-const scenarioPanels = ["panelCard1", "panelCard2", "panelCard3", "panelCard4", "panelCard5"];
+const scenarioPanels = ["panelCard1", "panelCard2", "panelCard3", "panelCard4"];
 
 for (const scenarioPanel of scenarioPanels) {
     document.getElementById(scenarioPanel).addEventListener("click", scenarioCardClicked);
@@ -79,7 +79,7 @@ function selectScenarioCard(scenarioCardNumber) {
   <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
 </svg>`;
 
-  for (let i = 1; i <= 5; i++) {
+  for (let i = 1; i <= 4; i++) {
     const card = document.getElementById('panelCard' + i);
     const selectedScenario = document.getElementById('selectedScenario' + i);
     const arrowIcon = card.querySelector('.arrow-icon');
