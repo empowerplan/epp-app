60bb418da433fa5e6465353284ff699ec33a7ac8
josihoppe <116898820+josihoppe@users.noreply.github.com>
2024-03-25 15:56:27 +0100

    fixed card error, due to false iteration of scenario cards

--- digiplan/static/js/scenarios.js
+++ digiplan/static/js/scenarios.js
@@ -84,7 +84,7 @@ function selectScenarioCard(scenarioCardNumber) {
   <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
 </svg>`;
 
-  for (let i = 1; i <= 4; i++) {
+  for (let i = 1; i <= 3; i++) {
     const card = document.getElementById("panelCard" + i);
     const selectedScenario = document.getElementById("selectedScenario" + i);
     const arrowIcon = card.querySelector(".arrow-icon");
