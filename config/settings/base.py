ba3d0b42c41c430bbc48a2ab59f525ffd9e2d8ab
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2024-10-15 13:54:38 +0200

    Reactivate hovering of municipalities

--- config/settings/base.py
+++ config/settings/base.py
@@ -303,6 +303,8 @@ MAP_ENGINE_IMAGES = [
     setup.MapImage("pv_ground_hatch", "images/map_pv_ground_hatch.png"),
 ]
 
+MAP_ENGINE_REGIONS = ["municipality"]
+
 MAP_ENGINE_API_MVTS = {
     "region": [
         setup.MVTAPI("region_boundaries", "map", "RegionBoundaries", style="region_boundaries"),
