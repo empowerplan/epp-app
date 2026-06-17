3aae070431f16113a425b4d3d4a7e99729a26f83
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2026-06-17 13:58:47 +0200

    Remove cycle chart for heat storages and add new text for storage charts

--- digiplan/static/js/results.js
+++ digiplan/static/js/results.js
@@ -44,7 +44,6 @@ export const resultCharts = {
   "heat_decentralized": "heat_decentralized_chart",
   "battery_energy": "battery_energy_chart",
   "battery_capacity": "battery_capacity_chart",
-  "heatstorage_energy": "heatstorage_energy_chart",
   "heatstorage_capacity": "heatstorage_capacity_chart",
 };
 
